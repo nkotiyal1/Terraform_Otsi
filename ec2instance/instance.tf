4cee8cbe842e97e506c1cc6a7241f3e81b886450
nkotiyal1 <110096385+nkotiyal1@users.noreply.github.com>
2022-07-27 15:36:01 +0530

    Update instance.tf
    Added Nitin @ Line # 6

--- ec2instance/instance.tf
+++ ec2instance/instance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "web" {
   ami           = "${var.ami_id}"
   instance_type = "${var.instance_type}"
   subnet_id     = "${var.subnet_id}"
-
+#Nitin
   tags = {
     Name = "HelloWorld"
   }
